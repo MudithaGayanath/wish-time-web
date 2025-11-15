adf500656daf8e18bafb5976e0da07e86e0f89bb
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-15 18:45:43 +0530

    Task status service service created

---
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/service/TaskStatusService.java
@@ -0,0 +1,11 @@
+package lk.wishu.wish_time.service;
+
+import lk.wishu.wish_time.repository.TaskStatusRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskStatusService {
+    @Autowired
+    private TaskStatusRepo taskStatusRepo;
+}
