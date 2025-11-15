810784698cce9da8cecf8f3b3443f6df0cd33e5e
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-15 18:47:58 +0530

    Task types service service created and changed TaskType into TaskTypeRepo in reositpry package

--- wish-time-backend/src/main/java/lk/wishu/wish_time/repository/TaskTypeRepo.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/repository/TaskTypeRepo.java
@@ -2,5 +2,5 @@ package lk.wishu.wish_time.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskType extends JpaRepository<TaskType,Integer> {
+public interface TaskTypeRepo extends JpaRepository<TaskTypeRepo,Integer> {
 }
