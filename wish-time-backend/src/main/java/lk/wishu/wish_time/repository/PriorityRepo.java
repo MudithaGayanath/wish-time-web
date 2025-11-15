ff65422f108dcf383b199b49bf7d4882d25db5a1
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-15 18:43:29 +0530

    Priority service created

--- wish-time-backend/src/main/java/lk/wishu/wish_time/repository/PriorityRepo.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/repository/PriorityRepo.java
@@ -3,5 +3,6 @@ package lk.wishu.wish_time.repository;
 import lk.wishu.wish_time.entity.Priority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PrirityRepo extends JpaRepository<Priority,Integer> {
+public interface PriorityRepo extends JpaRepository<Priority,Integer> {
+
 }
---
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/service/PriorityService.java
@@ -0,0 +1,10 @@
+package lk.wishu.wish_time.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PriorityService {
+    @Autowired
+    private  PriorityService priorityService;
+}
