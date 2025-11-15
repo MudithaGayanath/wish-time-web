5245cb7ad28ba7e9eb25d55de70b0dfedfdf4fd2
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-15 19:16:01 +0530

    Created method for get all priorities

--- wish-time-backend/src/main/java/lk/wishu/wish_time/service/PriorityService.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/service/PriorityService.java
@@ -1,11 +1,23 @@
 package lk.wishu.wish_time.service;
 
+import lk.wishu.wish_time.entity.Priority;
 import lk.wishu.wish_time.repository.PriorityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PriorityService {
+
+    private static final String HIGH = "High";
+    private static final String MEDIUM = "Medium";
+    private static final String LOW = "Low";
+
     @Autowired
     private PriorityRepo priorityRepo;
+
+    public List<Priority> getAll() {
+        return priorityRepo.findAll();
+    }
 }
