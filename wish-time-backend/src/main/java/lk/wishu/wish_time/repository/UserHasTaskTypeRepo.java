5c1d30bb039519e80b0bf9ec0b4d2ffe7cf40a1f
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-15 18:50:34 +0530

    UserHasTaskTypeSerivce created and changed UserHasTaskType into UserHasTaskTypeRepo in repository package

--- wish-time-backend/src/main/java/lk/wishu/wish_time/repository/UserHasTaskTypeRepo.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/repository/UserHasTaskTypeRepo.java
@@ -2,5 +2,5 @@ package lk.wishu.wish_time.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserHasTaskType extends JpaRepository<UserHasTaskType,Integer> {
+public interface UserHasTaskTypeRepo extends JpaRepository<UserHasTaskTypeRepo,Integer> {
 }
