c9cb9e5211b5af25e788ab524758bf323ee0ff1d
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-17 15:46:49 +0530

    Created method for get task status by name

--- wish-time-backend/src/main/java/lk/wishu/wish_time/repository/TaskStatusRepo.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/repository/TaskStatusRepo.java
@@ -3,6 +3,8 @@ package lk.wishu.wish_time.repository;
 import lk.wishu.wish_time.entity.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskStatusRepo extends JpaRepository<TaskStatus,Integer> {
+import java.util.Optional;
 
+public interface TaskStatusRepo extends JpaRepository<TaskStatus, Integer> {
+    Optional<TaskStatus> findByName(String name);
 }
