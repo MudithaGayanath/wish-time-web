3f940fc80801abfc5732818c2a19c4c3fcb525fe
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-17 15:47:13 +0530

    Created method for get task status by name

--- wish-time-backend/src/main/java/lk/wishu/wish_time/service/TaskService.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/service/TaskService.java
@@ -1,11 +1,75 @@
 package lk.wishu.wish_time.service;
 
+import lk.wishu.wish_time.dto.request.TaskRequest;
 import lk.wishu.wish_time.repository.TaskRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class TaskService {
     @Autowired
+    @Lazy
     private TaskRepo taskRepo;
+    @Autowired
+    @Lazy
+    private JWTService jwtService;
+    @Autowired
+    @Lazy
+    private UserService userService;
+    @Autowired
+    @Lazy
+    private TaskTypeService taskTypeService;
+
+
+
+    public void insert(String token, TaskRequest data){
+        HashMap<String,String> errors = this.validate(data);
+    }
+
+    private HashMap<String, String> validate(TaskRequest data) {
+        HashMap<String, String> map = new HashMap<>();
+//        validate title
+        if(data.getTitle() == null || data.getTitle().isBlank()){
+            map.put("title","Title required");
+        } else if (data.getTitle().length() > 45) {
+            map.put("title","Title can't be longer than 45 characters");
+        }
+
+//        validate description
+        if(data.getDescription() != null || !data.getDescription().isBlank()){
+            if(data.getDescription().length() > 500){
+                map.put("description","Description can't be longer than 500 characters");
+            }
+        }
+
+//        validate estimated time
+        if(data.getEstimatedTime() == null || data.getEstimatedTime().isBlank()){
+            map.put("estimatedTime","Estimated time required");
+        }else {
+            try {
+                if(Double.parseDouble(data.getEstimatedTime()) <= 0){
+                    map.put("estimatedTime","Estimated time must be greater than 0");
+                }
+            } catch (NumberFormatException e) {
+                map.put("estimatedTime","Estimated time must be a number");
+            }
+        }
+// validate task id
+        if(data.getTaskTypeId() == null || data.getTaskTypeId().isBlank()){
+            map.put("taskTypeId","Task type required");
+        }else{
+            try {
+                if(!taskTypeService.isValidId(Integer.parseInt(data.getTaskTypeId()))){
+                    map.put("taskTypeId","Invalid task type");
+                }
+            }catch (NumberFormatException e){
+                map.put("taskTypeId","Task type must be a number");
+            }
+        }
+
+        return map;
+    }
 }
--- wish-time-backend/src/main/java/lk/wishu/wish_time/service/TaskStatusService.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/service/TaskStatusService.java
@@ -1,13 +1,24 @@
 package lk.wishu.wish_time.service;
 
+import lk.wishu.wish_time.entity.TaskStatus;
 import lk.wishu.wish_time.repository.TaskStatusRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskStatusService {
+    public static final String  PENDING = "Pending";
+    public static final String  IN_PROGRESS = "In Progress";
+    public static final String  COMPLETED = "Completed";
+    public static final String  SCHEDULED = "Scheduled";
+    public static final String  IN_COMPLETE = "In Complete";
+
+
     @Autowired
     private TaskStatusRepo taskStatusRepo;
 
+    protected TaskStatus getTaskStatusByName(String name){
+        return this.taskStatusRepo.findByName(name).orElse(null);
+    }
 
 }
