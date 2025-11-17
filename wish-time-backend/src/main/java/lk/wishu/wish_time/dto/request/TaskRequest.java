c718373c9e0443697e0f8f2bcf3cc9022a286481
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-17 12:14:46 +0530

    Created class for Tast request

---
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/dto/request/TaskRequest.java
@@ -0,0 +1,16 @@
+package lk.wishu.wish_time.dto.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+public class TaskRequest {
+    private String title;
+    private String description;
+    private String estimatedTime;
+    private String taskTypeId;
+    private String statusId;
+    private String priorityId;
+}
