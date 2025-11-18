e392d73e5cd324cfd215b72f5275af3aafbe04ff
Muditha Gayanath <mudithawijesigha@gmail.com>
2025-11-19 00:08:56 +0530

    Changed schedule response list in to hashmap

--- wish-time-backend/src/main/java/lk/wishu/wish_time/dto/response/TaskScheduleResponse.java
+++ wish-time-backend/src/main/java/lk/wishu/wish_time/dto/response/TaskScheduleResponse.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 @Data
 @Getter @Setter
 public class TaskScheduleResponse {
-    private List<TaskResponse> tasks;
+    private HashMap<LocalDate, List<TaskResponse>> tasks;
 }
