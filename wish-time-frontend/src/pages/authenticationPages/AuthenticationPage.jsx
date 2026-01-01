6173c37e5b244b2e28e412349eaf7fd18ddf76d5
Muditha Gayanath <mudithawijesigha@gmail.com>
2026-01-01 19:25:50 +0530

    add navigation to auth page

--- wish-time-frontend/src/pages/authenticationPages/AuthenticationPage.jsx
+++ wish-time-frontend/src/pages/authenticationPages/AuthenticationPage.jsx
@@ -1,21 +1,35 @@
 import SignInPage from "./SignInPage.jsx";
 import SmallButton from "../../components/buttons/SmallButton.jsx";
-import React from "react";
+import React, {useState} from "react";
 import SignUpPage from "./SignUpPage.jsx";
 
 export default function AuthenticationPage() {
+    const [activePage, setActivePage] = useState(<SignInPage/>)
+
+    function handleSignInPage() {
+        setActivePage(<SignInPage/>)
+    }
+    function handleSignUpPage() {
+        setActivePage(<SignUpPage/>)
+    }
+
     return (
-        <div className="w-full    ">
-            <div className="grid grid-cols-5 h-screen bg-red-400">
-
-                <div className="col-span-1"></div>
-                <div className="col-span-3 flex justify-center items-center bg-white">
-                    <SignInPage />
-                </div>
-                <div className="col-span-1"></div>
-
+        <div className="w-full  h-screen  ">
+        {/*   auth nav start*/}
+            <div className=" w-full h-1/5 flex items-center justify-center gap-4">
+                <button className="bg-purple-500 h-12 w-20  rounded-s-2xl text-white"
+                onClick={handleSignInPage}>Sign In</button>
+                <button
+                onClick={handleSignUpPage}
+                >Sign Up</button>
             </div>
+        {/*   auth nav end*/}
 
+        {/*    element start*/}
+            <div className="w-full h-4/5 flex items-center justify-center">
+                {activePage}
+            </div>
+        {/*    element end*/}
         </div>
     );
 }
