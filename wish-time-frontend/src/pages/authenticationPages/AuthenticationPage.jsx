6dfbea74c98ff1d76a5d0b0cdffce70a1ea58bb8
Muditha Gayanath <mudithawijesigha@gmail.com>
2026-01-02 10:41:03 +0530

    bug fiexed in auth page

--- wish-time-frontend/src/pages/authenticationPages/AuthenticationPage.jsx
+++ wish-time-frontend/src/pages/authenticationPages/AuthenticationPage.jsx
@@ -15,6 +15,7 @@ export default function AuthenticationPage() {
     function handleSignUpPage() {
         setActivePage("sign-up");
         setActiveElement(<SignUpPage navigation={() => {
+            setActivePage("sign-up");
             setActiveElement(<SignInPage/>)
         }}/>)
     }
